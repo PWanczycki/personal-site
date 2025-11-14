bf9c1b24ad5747d6395751f8567477b3b921953e
PWanczycki <p.wanczycki@gmail.com>
2025-11-13 22:20:53 -0500

    migrate home page to react

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    render inertia: "Home/Home", props: {name: params[:name]}
   end
 end
---
+++ app/javascript/pages/Home/Home.tsx
@@ -0,0 +1,32 @@
+import {Head} from "@inertiajs/react";
+
+interface HomeProps {
+  name?: string
+}
+
+function Home({ name }: HomeProps) {
+  return (
+    <div>
+      <Head title="Philip Wanczycki - Software Developer" />
+
+      <div>
+        <h1>Philip Wanczycki</h1>
+        {name && (
+          <p>
+            Hello {name}!
+          </p>
+        )}
+        <p>
+          I am a software developer with a variety of interests, including
+          artificial intelligence, cybersecurity, and web development.
+          <br />
+          <br />
+          Feel free to browse my projects below!
+        </p>
+        <a href="/projects">Projects</a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
